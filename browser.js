20815c3cdca745993a7026cefa1d3b836ba648f1
Richie Bendall <richiebendall@gmail.com>
2020-03-12 22:32:35 +1300

    Add browser support
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

---
+++ browser.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.write = async text => {
+	await navigator.clipboard.writeText(text)
+}
+
+exports.read = async () => {
+	return await navigator.clipboard.readText()
+}
+
+exports.readSync = () => {
+	throw new Error("readSync not supported in browsers!")
+}
+
+exports.writeSync = () => {
+	throw new Error("writeSync not supported in browsers!")
+}
