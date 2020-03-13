df5bee3dc87d43600eef2e783686ef44bbedc16d
Richie Bendall <richiebendall@gmail.com>
2020-03-14 07:47:13 +1300

    chore: Fix lint
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- browser.js
+++ browser.js
@@ -1,17 +1,17 @@
 'use strict';
 
 exports.write = async text => {
-	await navigator.clipboard.writeText(text)
-}
+	await navigator.clipboard.writeText(text);
+};
 
 exports.read = async () => {
-	return await navigator.clipboard.readText()
-}
+	return navigator.clipboard.readText();
+};
 
 exports.readSync = () => {
-	throw new Error("readSync not supported in browsers!")
-}
+	throw new Error('readSync not supported in browsers!');
+};
 
 exports.writeSync = () => {
-	throw new Error("writeSync not supported in browsers!")
-}
+	throw new Error('writeSync not supported in browsers!');
+};
