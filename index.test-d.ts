ac83ec4cf0c967e7e336f7e41eef64b13af7fc25
Sindre Sorhus <sindresorhus@gmail.com>
2025-09-26 17:29:03 +0900

    Require Node.js 20

--- index.test-d.ts
+++
@@ -1,7 +0,0 @@
-import {expectType} from 'tsd';
-import clipboard from './index.js';
-
-clipboard.writeSync('🦄');
-expectType<Promise<void>>(clipboard.write('🦄'));
-expectType<string>(clipboard.readSync());
-expectType<Promise<string>>(clipboard.read());
